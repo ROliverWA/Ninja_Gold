1b4ec6cfb30e00e413124e68a5d180a8f59b9973
Robert Oliver <ROliverWA@Outlook.com>
2019-04-02 17:10:35 -0700

    layout and server basic setup

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+app.secret_key="thekeytomyheart"
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    app.run(debug = True)
