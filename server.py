326bc1fd1f567376c07420312f2afc540c39a448
Robert Oliver <ROliverWA@Outlook.com>
2019-04-02 18:56:21 -0700

    implemented CSS/Finished GUI

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,request,url_for
 import random
 app = Flask(__name__)
 app.secret_key="thekeytomyheart"
+gold = 0
 
 @app.route('/')
 def root():
+    session['my_trust'] = gold
     return render_template('index.html')
     
-    
-    
+@app.route('/process_money', methods=['POST'])
+def shake_your_moneymaker():
+    return render_template('index.html')
     
     
     
